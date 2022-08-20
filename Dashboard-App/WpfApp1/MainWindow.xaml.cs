a833310c1dfab5a23c76d6d205211f7bfc168020
Johnny Chai/WYHQ/Wiwynn <Johnny_Chai@wiwynn.com>
2022-08-21 00:28:22 +0800

    20220821-0005

--- Dashboard-App/WpfApp1/MainWindow.xaml.cs
+++ Dashboard-App/WpfApp1/MainWindow.xaml.cs
@@ -31,16 +31,57 @@ namespace WpfApp1
             {
                 this.DragMove();
             }
-
         }
 
         private void ControlBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (sender == BtnPower)
+            PageDashBoard.Visibility = Visibility.Hidden;
+            PageListing.Visibility = Visibility.Hidden;
+            PageMessage.Visibility = Visibility.Hidden;
+            PageOrders.Visibility = Visibility.Hidden;
+            PageStatistics.Visibility = Visibility.Hidden;
+            PageRevenue.Visibility = Visibility.Hidden;
+            PagePromete.Visibility = Visibility.Hidden;
+            PageSetting.Visibility = Visibility.Hidden;
+
+            if (sender == BtnDashBoard)
+            {
+                PageDashBoard.Visibility = Visibility.Visible;
+                BtnDashBoard.Style = (Style)Resources["menuButtonActive"];
+                    }
+            else if (sender == BtnListing)
+            {
+                PageListing.Visibility = Visibility.Visible;
+                BtnListing.Style = (Style)Resources["menuButtonActive"];
+            }
+            else if (sender == BtnMessage)
+            {
+                PageMessage.Visibility = Visibility.Visible;
+            }
+            else if (sender == BtnOrders)
+            {
+                PageOrders.Visibility = Visibility.Visible;
+            }
+            else if (sender == BtnStatistics)
+            {
+                PageStatistics.Visibility = Visibility.Visible;
+            }
+            else if (sender == BtnRevenue)
+            {
+                PageRevenue.Visibility = Visibility.Visible;
+            }
+            else if (sender == BtnPromote)
+            {
+                PagePromete.Visibility = Visibility.Visible;
+            }
+            else if (sender == BtnSetting)
+            {
+                PageSetting.Visibility = Visibility.Visible;
+            }
+            else if (sender == BtnPower)
             {
                 Application.Current.Shutdown();
             }
-
         }
     }
 }
